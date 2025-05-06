fa733e26991265a8615145a9494fb60d84b276b9
Abhishek Das <abhishek.das11@gds.ey.com>
2025-05-07 01:17:38 +0530

    "2nd commit"

--- src/main/java/epcentral/Main.java
+++ src/main/java/epcentral/Main.java
@@ -3,12 +3,13 @@ package epcentral;
 import epcentral.Utility.ReadProperties;
 import epcentral.data.DAO;
 
-import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static epcentral.view.ViewRecords.viewRecord;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         DAO dao = new DAO(ReadProperties.getEnvironmentName(args[0]));
-        dao.insertIntoTable(7,"bb",14);
-
+        viewRecord(dao.getAllRecords());
     }
 }
--- src/main/java/epcentral/view/ViewRecords.java
+++ src/main/java/epcentral/view/ViewRecords.java
@@ -1,5 +1,15 @@
 package epcentral.view;
 
-public class ViewRecords {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class ViewRecords {
+     public static void viewRecord(ResultSet resultSet) throws SQLException {
+         System.out.println("-|----------------------------|-");
+         while(resultSet.next()){
+             System.out.println("ID "+resultSet.getInt(1));
+             System.out.println("Name "+resultSet.getString(2));
+             System.out.println("Age "+resultSet.getInt(3));
+         }
+     }
 }
