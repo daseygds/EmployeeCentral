5df74d3b54d4337efa3c56b9e783ac10788cf65c
ABHISHEK DAS <abhishek.das.docker@gmail.com>
2025-05-11 00:06:56 +0530

    feature: connect db to ui

--- src/main/java/epcentral/Main.java
+++ src/main/java/epcentral/Main.java
@@ -5,10 +5,12 @@ import epcentral.view.EmployeeForm;
 import javax.swing.*;
 import java.sql.SQLException;
 
+import static epcentral.Utility.ReadProperties.getEnvironmentName;
+
 public class Main {
     public static void main(String[] args) throws SQLException {
         SwingUtilities.invokeLater(() -> {
-            EmployeeForm form = new EmployeeForm();
+            EmployeeForm form = new EmployeeForm(getEnvironmentName(args[0]));
             form.setVisible(true);
         });
     }
